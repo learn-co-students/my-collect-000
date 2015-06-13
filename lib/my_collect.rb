a15821a609548017a3fddf6f500b9c39798d3607
plai217 <philiplai217@gmail.com>
2015-06-12 20:27:49 -0400

    done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array) # put argument(s) here
   # code here
   new_array = []
   for i in 0..array.length - 1
-    new_array << array[i].upcase
+    new_array << yield(array[i])
   end
   return new_array
 end
