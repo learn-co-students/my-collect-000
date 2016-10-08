7e8a4c485e914c632b659f3ee8bc0b65aea3e8d5
Helga108 <helgamir@gmail.com>
2016-10-08 20:20:42 +0300

    done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(collection)
+	result = Array.new
+	finish = collection.length
+	count = 0
+	while count < finish
+		m = yield collection[count]
+		result << m
+		count = count + 1	
+	end
+	return result
+	end
 
